ad3d11ac830cf8c30db27d7a05fe9b1e130faaea
Onkar Birajdar <67547781+Onkar-Birajdar@users.noreply.github.com>
2022-04-18 01:11:07 +0530

    Added new colors and features'

--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -4,16 +4,24 @@ function Card() {
     return (
         <div className="card-wrapper">
             <div className="card">
-                <h3 className="card-content">New Note</h3>
+                <h3 className="card-content" contentEditable="true">
+                    New Note
+                </h3>
             </div>
             <div className="card">
-                <h3 className="card-content">New Note</h3>
+                <h3 className="card-content" contentEditable="true">
+                    New Note
+                </h3>
             </div>
             <div className="card">
-                <h3 className="card-content">New Note</h3>
+                <h3 className="card-content" contentEditable="true">
+                    New Note
+                </h3>
             </div>
             <div className="card">
-                <h3 className="card-content">New Note</h3>
+                <h3 className="card-content" contentEditable="true" >
+                    New Note
+                </h3>
             </div>
         </div>
     );
