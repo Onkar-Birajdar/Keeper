f601ee5dacf8d7d25e8c7b8da539cd210c212464
Onkar Birajdar <67547781+Onkar-Birajdar@users.noreply.github.com>
2022-04-20 20:25:16 +0530

    Getting file back

--- src/Components/card.jsx
+++ src/Components/card.jsx
@@ -5,30 +5,34 @@ function Card() {
         <div className="card-wrapper">
             <div className="card">
                 <h3 className="card-content" contentEditable="true">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt ab ipsam sapiente dignissimos aut nihil? Impedit, ducimus? Quod, porro odit, repudiandae tenetur quis quo temporibus odio hic, dolor id libero.
-    Non illum, praesentium illo enim voluptas quisquam cumque fugit, reiciendis ullam aperiam, magnam quaerat culpa. Maiores ratione officia nostrum debitis eos labore aliquam aperiam, dolorum itaque, quia temporibus vitae explicabo.
-    Harum quisquam illum odio natus vero accusantium inventore non veniam deserunt consectetur alias enim fugit libero delectus, recusandae iusto doloribus at? Itaque obcaecati cum iure illo voluptates magni distinctio officiis!
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Sunt ab ipsam sapiente dignissimos aut nihil? Impedit,
+                    ducimus? Quod, porro odit, repudiandae tenetur quis quo
+                    temporibus odio hic, dolor id libero.
                 </h3>
             </div>
             <div className="card">
                 <h3 className="card-content" contentEditable="true">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt ab ipsam sapiente dignissimos aut nihil? Impedit, ducimus? Quod, porro odit, repudiandae tenetur quis quo temporibus odio hic, dolor id libero.
-    Non illum, praesentium illo enim voluptas quisquam cumque fugit, reiciendis ullam aperiam, magnam quaerat culpa. Maiores ratione officia nostrum debitis eos labore aliquam aperiam, dolorum itaque, quia temporibus vitae explicabo.
-    Harum quisquam illum odio natus vero accusantium inventore non veniam deserunt consectetur alias enim fugit libero delectus, recusandae iusto doloribus at? Itaque obcaecati cum iure illo voluptates magni distinctio officiis!
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Sunt ab ipsam sapiente dignissimos aut nihil? Impedit,
+                    ducimus? Quod, porro odit, repudiandae tenetur quis quo
+                    temporibus odio hic, dolor id libero.
                 </h3>
             </div>
             <div className="card">
                 <h3 className="card-content" contentEditable="true">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt ab ipsam sapiente dignissimos aut nihil? Impedit, ducimus? Quod, porro odit, repudiandae tenetur quis quo temporibus odio hic, dolor id libero.
-    Non illum, praesentium illo enim voluptas quisquam cumque fugit, reiciendis ullam aperiam, magnam quaerat culpa. Maiores ratione officia nostrum debitis eos labore aliquam aperiam, dolorum itaque, quia temporibus vitae explicabo.
-    Harum quisquam illum odio natus vero accusantium inventore non veniam deserunt consectetur alias enim fugit libero delectus, recusandae iusto doloribus at? Itaque obcaecati cum iure illo voluptates magni distinctio officiis!
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Sunt ab ipsam sapiente dignissimos aut nihil? Impedit,
+                    ducimus? Quod, porro odit, repudiandae tenetur quis quo
+                       temporibus odio hic, dolor id libero.
                 </h3>
             </div>
             <div className="card">
                 <h3 className="card-content" contentEditable="true">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sunt ab ipsam sapiente dignissimos aut nihil? Impedit, ducimus? Quod, porro odit, repudiandae tenetur quis quo temporibus odio hic, dolor id libero.
-    Non illum, praesentium illo enim voluptas quisquam cumque fugit, reiciendis ullam aperiam, magnam quaerat culpa. Maiores ratione officia nostrum debitis eos labore aliquam aperiam, dolorum itaque, quia temporibus vitae explicabo.
-    Harum quisquam illum odio natus vero accusantium inventore non veniam deserunt consectetur alias enim fugit libero delectus, recusandae iusto doloribus at? Itaque obcaecati cum iure illo voluptates magni distinctio officiis!
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                    Sunt ab ipsam sapiente dignissimos aut nihil? Impedit,
+                    ducimus? Quod, porro odit, repudiandae tenetur quis quo
+                    temporibus odio hic, dolor id libero.
                 </h3>
             </div>
         </div>
